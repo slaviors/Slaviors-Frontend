53e3ee19a35a0bdb8bf8a151b90bccdf82a146a9
mamatqurtifa <mqurtifawijaya007@gmail.com>
2025-01-04 20:21:29 +0700

    mamat -1st push

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,11 +1,13 @@
+import Image from "next/image";
+
 export default function HomeSection() {
     return (
         <article className="p-20 w-full h-screen">
             <div class="flex items-center mb-4">
-                <image class="w-10 h-10 me-4 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt=""></image>
-                    <div class="font-medium dark:text-white">
-                        <p>Jese Leos <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
-                    </div>
+            <Image class="w-10 h-10 me-4 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                <div class="font-medium dark:text-white">
+                    <p>Jese Leos <time datetime="2014-08-16 19:00" class="block text-sm text-gray-500 dark:text-gray-400">Joined on August 2014</time></p>
+                </div>
             </div>
             <div class="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
                 <svg class="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
